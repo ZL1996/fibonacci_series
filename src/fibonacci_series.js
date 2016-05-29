dca147d36b1afc43cf7adc81d0380fe4dbc428b7
ZL1996 <649349296@qq.com>
2016-05-29 19:12:20 +0800

    Update fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,15 @@
 'use strict';
-
 function fibonacci_series(n) {
-
+    var a = [0,1];
+    var result = fun(n-1,a);
+    return result;
 }
 
+function fun(n,a){
+  if (n == 0)
+	return a;
+  a.push(a[a.length-1]+a[a.length-2]);
+  fun(n-1,a);
+  return a;
+}
 module.exports = fibonacci_series;
